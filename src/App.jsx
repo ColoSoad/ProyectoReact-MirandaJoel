23bd8cf11d837cd14faee6fecf4a542fa1c229ab
Joel <joelhernanmiranda@gmail.com>
2024-03-05 15:53:10 -0300

    Primer commit de proyecto con reactJs

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { titulo } from './components/titulo';
+
+function App() {
+    return (
+        <main>
+            <div>
+                <p>
+                    <titulo />
+                </p>
+            </div>
+        </main>
+    );
+}
+
+export default App;
